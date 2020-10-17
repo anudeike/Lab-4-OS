0bab3bf09803a17d2108a487f58a98e455698c3d
Ikechukwu <ikechukwuanude@gmail.com>
2020-10-17 03:53:13 +0000

    filled in task 2

--- task2/task2.c
+++ task2/task2.c
@@ -3,14 +3,53 @@
 
 /* IMPLEMENT ME: Declare your functions here */
 int add (int a, int b);
+int subtract (int a, int b);
+int multiply (int a, int b);
+int divide (int a, int b);
+int end();
 
+/* IMPLEMENT ME: Declare your functions here */
+int add (int a, int b);
+
+/* IMPLEMENT ME: Define your functions here */
+// functions stubs, will not be able to call from bottom
+int add (int a, int b) { printf ("Adding 'a' and 'b'\n"); return a + b; }
+
+int subtract (int a, int b) { printf ("Subtracting 'a' and 'b'\n"); return a - b; }
+
+int multiply (int a, int b) { printf ("Multiplying 'a' and 'b'\n"); return a * b; }
+
+int divide (int a, int b) { printf ("Dividing 'a' and 'b'\n"); return a / b; }
+
+int end() { printf ("Exit.\n"); exit(0); }
+
+
+// functions pointers
+int (*ops[5]) (int a, int b);
 
 int main (void)
 {
 	/* IMPLEMENT ME: Insert your algorithm here */
+  int op1, op2, operation; 
+	
+	printf("Operand 'a': ");
+	scanf("%d", &op1);
+	printf("Operand 'b': ");
+	scanf("%d", &op2);
+	printf("Choose operation to perform (0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4 : exit):\n");
+	scanf("%d", &operation);
+	
+	ops[0] = add;
+	ops[1] = subtract;
+	ops[2] = multiply;
+	ops[3] = divide;
+	ops[4] = end;
+
+	int result = (*ops[operation]) (op1, op2);
+	printf("x = %d\n", result);
+
+	return result;
 
 	return 0;
 }
 
-/* IMPLEMENT ME: Define your functions here */
-int add (int a, int b) { printf ("Adding 'a' and 'b'\n"); return a + b; }
